7c9fec2422d9270d3b1a0972eb61badd2a1dc31f
Nina Loeterman <nina.loeterman@gmail.com>
2021-08-20 21:03:02 +0300

    cleanup

--- singly_linked_list.js
+++ singly_linked_list.js
@@ -119,21 +119,21 @@ class SinglyLinkedList {
   }
 
   reverse() {
-      //swap head and tail
-      let node = this.head;
-      this.head = this.tail;
-      this.tail = node;
+    //swap head and tail
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
 
-      let prev = null;
-      let next = null;
+    let prev = null;
+    let next = null;
 
-      for(let i = 0; i < this.length; i++) {
-        next = node.next;
-        node.next = prev
-        prev = node;
-        node = next;
-      }
-      return this
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
   }
 }
 
