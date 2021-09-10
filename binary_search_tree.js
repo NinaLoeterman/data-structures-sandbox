683085ae0502edbb662ea5e48bad8e176317f029
Nina Loeterman <nina.loeterman@gmail.com>
2021-09-10 22:09:25 +0300

    implement insert and find

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -10,4 +10,44 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(val) {
+    const node = new Node(val);
+    if (!this.root) {
+      this.root = node;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (val === current.val) return undefined;
+      if (val < current.val) {
+        if (!current.left) {
+          current.left = node;
+          return this;
+        }
+        current = current.left;
+      } else if (val > current.val) {
+        if (!current.right) {
+          current.right = node;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+
+  find(val) {
+    if (!this.root) return false;
+    let current = this.root;
+    while (true) {
+      if (val === current.val) return current;
+      if (val < current.val) {
+        if (!current.left) return false;
+        else current = current.left;
+      } else if (val > current.val) {
+        if (!current.right) return false;
+        else current = current.right;
+      }
+    }
+  }
 }
