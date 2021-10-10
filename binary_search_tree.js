305fd63878614cdd1c242bc6409ea563d0523fe0
Nina Loeterman <nina.loeterman@gmail.com>
2021-10-10 19:47:44 +0300

    bfs and dfs

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -50,4 +50,82 @@ class BinarySearchTree {
       }
     }
   }
+
+  contains(val) {
+    if (!this.root) return false;
+    let current = this.root,
+      found = false;
+    while (current && !found) {
+      if (val < current.val) {
+        current = current.left;
+      } else if (val > current.val) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  BFS() {
+    var node = this.root,
+      data = [],
+      queue = [];
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+
+  DFSPreOrder() {
+    let data = [];
+    const traverse = (node) => {
+      data.push(node.val);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    };
+    traverse(this.root);
+    console.log("DFS pre order: ", data);
+    return data;
+  }
+
+  DFSPostOrder() {
+    let data = [];
+    const traverse = (node) => {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.val);
+    };
+    traverse(this.root);
+    console.log("DFS post order: ", data);
+    return data;
+  }
+  DFSInOrder() {
+    let data = [];
+    const traverse = (node) => {
+      if (node.left) traverse(node.left);
+      data.push(node.val);
+      if (node.right) traverse(node.right);
+    };
+    traverse(this.root);
+    console.log("DFS in order: ", data);
+    return data;
+  }
 }
+
+const tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+tree.BFS();
+tree.DFSPostOrder();
+tree.DFSPreOrder();
+tree.DFSInOrder();
