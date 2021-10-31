a555b9c014745de679e636cb2b38b28a116cf682
Nina Loeterman <nina.loeterman@gmail.com>
2021-10-31 23:08:03 +0200

    extract max in binary heap

--- max_binary_heap.js
+++ max_binary_heap.js
@@ -6,27 +6,68 @@ class MaxBinaryHeap {
   insert(element) {
     this.values.push(element);
     this.bubbleUp(element);
-    console.log(this.values)
+  }
+
+  extractMax() {
+    if (this.values.length < 2) return undefined;
+    const max = this.values.shift();
+    const newRoot = this.values.pop();
+    this.values.unshift(newRoot);
+
+    this.bubbleDown(newRoot);
+    return max;
   }
 
   bubbleUp(element) {
     let currentIndex = this.values.length - 1;
-    let parentIndex = this.findParentIndex(currentIndex);
+    let parentIndex = this.parentIdx(currentIndex);
     let parent = this.values[parentIndex];
 
     while (parentIndex >= 0 && element > parent) {
       this.values[parentIndex] = element;
       this.values[currentIndex] = parent;
       currentIndex = parentIndex;
-      parentIndex = this.findParentIndex(currentIndex);
+      parentIndex = this.parentIdx(currentIndex);
       parent = this.values[parentIndex];
     }
   }
 
-  findParentIndex(index) {
+  bubbleDown(element) {
+    let currentIndex = 0;
+    let leftChildIndex = this.leftChildIdx(currentIndex);
+    let rightChildIndex = this.rightChildIdx(currentIndex);
+    let leftChild = this.values[leftChildIndex];
+    let rightChild = this.values[rightChildIndex];
+
+    while (element < leftChild || element < rightChild) {
+      if (leftChild > rightChild) {
+        this.values[currentIndex] = leftChild;
+        this.values[leftChildIndex] = element;
+        currentIndex = leftChildIndex;
+      } else {
+        this.values[currentIndex] = rightChild;
+        this.values[rightChildIndex] = element;
+        currentIndex = rightChildIndex;
+      }
+      leftChildIndex = this.leftChildIdx(currentIndex);
+      rightChildIndex = this.rightChildIdx(currentIndex);
+      leftChild = this.values[leftChildIndex];
+      rightChild = this.values[rightChildIndex];
+    }
+  }
+
+  leftChildIdx(index) {
+    return index * 2 + 1;
+  }
+
+  rightChildIdx(index) {
+    return index * 2 + 2;
+  }
+
+  parentIdx(index) {
     return Math.floor((index - 1) / 2);
   }
 }
 
 let heap = new MaxBinaryHeap();
-heap.insert(37);
+heap.extractMax();
